0a663e855c4623955c9b3de797e4c9f4e3587c28
Santiago <monokutho@gmail.com>
2022-02-06 17:12:36 +0100

    HeroImage Styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React, { useState , useEffect } from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
+import HeroImage from './HeroImage';
+
 import { useHomeFetch } from '../hooks/useHomeFetch';
 
 import NoImage from '../images/no_image.jpg';
@@ -8,7 +10,18 @@ import NoImage from '../images/no_image.jpg';
 const Home = () => {
     const { state, loading, error } = useHomeFetch();
     console.log(state);
-    return <div>Home Page</div>
+    return (
+        <>
+            {state.results[0] ? (
+                <HeroImage 
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                />
+            ) : null
+            }
+        </>
+    )
 }
 
 export default Home;
