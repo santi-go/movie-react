28bfa4d2a27b2d5a658ed0a20691b887f15c40b0
Santiago <monokutho@gmail.com>
2022-02-06 11:51:04 +0100

    UseEffect and fetching data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Header from './components/Header'
+import Header from './components/Header';
+import Home from './components/Home';
 
 import { GlobalStyle } from './GlobalStyle'
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      Start here.
+      <Home />
       <GlobalStyle /> 
     </div>
   );
