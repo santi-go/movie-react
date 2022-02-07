fb36899feeec8de1ff93047201bef71ed9a8271d
Santiago <monokutho@gmail.com>
2022-02-07 23:45:51 +0100

    SearchBar Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,40 +5,42 @@ import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 
 import { useHomeFetch } from '../hooks/useHomeFetch';
 
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const { state, loading, error } = useHomeFetch();
-    console.log(state);
-    return (
-        <>
-            {state.results[0] ? (
-                <HeroImage 
-                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-                    title={state.results[0].original_title}
-                    text={state.results[0].overview}
-                />
-            ) : null}
-            <Grid header='Popular Movies'>
-                {state.results.map(movie => (
-                    <Thumb
-                    key={movie.id}
-                    clickable
-                    image={
-                        movie.poster_path 
-                            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
-                            : NoImage
-                    } 
-                    movieId={movie.id}
-                    />
-                ))}
-            </Grid>
-            <Spinner />
-        </>
-    )
+  const { state, loading, error, setSearchTerm } = useHomeFetch();
+  console.log(state);
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <Grid header='Popular Movies'>
+        {state.results.map(movie => (
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                : NoImage
+            }
+            movieId={movie.id}
+          />
+        ))}
+      </Grid>
+      <Spinner />
+    </>
+  )
 }
 
 export default Home;
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -2,38 +2,39 @@ import { useState, useEffect, useRef } from 'react';
 import API from '../API';
 
 const initialState = {
-    page: 0,
-    results: [],
-    total_pages: 0,
-    total_results: 0
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0
 }
 
 export const useHomeFetch = () => {
-    const [ state, setState ] = useState(initialState);
-    const [ loading, setLoading ] = useState(false);
-    const [ error, setError ] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [state, setState] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-    const fetchMovies = async (page, searchTerm = "") => {
-        try {
-            setError(false);
-            setLoading(true);
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
 
-            const movies = await API.fetchMovies(searchTerm, page);
-            
-            setState(prev => ({
-                ...movies,
-                results:
-                page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-            }));
-        } catch (error) {
-            setError(true);
-        }
-        setLoading(false);
-    };
+      const movies = await API.fetchMovies(searchTerm, page);
 
-    useEffect(() => {
-        fetchMovies(1)
-    }, []);
+      setState(prev => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
 
-    return { state, loading, error };
+  useEffect(() => {
+    fetchMovies(1)
+  }, []);
+
+  return { state, loading, error, setSearchTerm };
 }
