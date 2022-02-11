943d19a73363a36da33cb0b51edcb4e3d1e63034
Santiago <monokutho@gmail.com>
2022-02-11 19:48:31 +0100

    Button Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,14 +6,24 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 
 import { useHomeFetch } from '../hooks/useHomeFetch';
 
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
-  console.log(state);
+  const { 
+    state, 
+    loading, 
+    error, 
+    searchTerm, 
+    setSearchTerm, 
+    setIsLoadingMore 
+  } = useHomeFetch();
+
+  if (error) return <div>Something went wrong. Please try again.</div>
+
   return (
     <>
       {!searchTerm && state.results[0] ? (
@@ -38,7 +48,10 @@ const Home = () => {
           />
         ))}
       </Grid>
-      <Spinner />
+      {loading && <Spinner />}
+      {state.page < state.total_pages && !loading && (
+        <Button text='Load More' callback={() => setIsLoadingMore(true)} />
+      )}
     </>
   )
 }
