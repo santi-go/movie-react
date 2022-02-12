3e6332ca3582338f401a95789565333e605d9d9b
Santiago <monokutho@gmail.com>
2022-02-12 15:33:25 +0100

    MovieInfo Component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,6 +6,7 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 import BreadCrumb from './BreadCrumb';
 import Grid from './Grid';
 import Spinner from './Spinner';
+import MovieInfo from './MovieInfo';
 
 import { useMovieFetch } from '../hooks/useMovieFetch';
 
@@ -21,6 +22,7 @@ const Movie = () => {
   return (
     <>
       <BreadCrumb movieTitle={movie.original_title}/>
+      <MovieInfo movie={movie}/>
     </>
   )
 };
