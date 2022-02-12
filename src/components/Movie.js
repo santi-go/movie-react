742c34867c625945effe22c917f16c28badf5c95
Santiago <monokutho@gmail.com>
2022-02-12 12:41:10 +0100

    BreadCrump Component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 
+import BreadCrumb from './BreadCrumb';
 import Grid from './Grid';
 import Spinner from './Spinner';
 
@@ -13,10 +14,13 @@ import NoImage from '../images/no_image.jpg';
 const Movie = () => {
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMovieFetch(movieId);
-  console.log(movie);
+
+  if (loading) return <Spinner />;
+  if (error) return <div>Somenthing went wrong...</div>
+
   return (
     <>
-      <div>MOvie</div>
+      <BreadCrumb movieTitle={movie.original_title}/>
     </>
   )
 };
