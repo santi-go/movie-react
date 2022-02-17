bcd28338e968ae941d0fe1542324c4cd1f952eda
Santiago <monokutho@gmail.com>
2022-02-17 19:59:14 +0100

    Session Storage Movie

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
 import API from '../API';
+import { isPersistedState } from '../helpers';
 
 export const useMovieFetch = movieId => {
   const [state, setState] = useState({});
@@ -30,8 +31,20 @@ export const useMovieFetch = movieId => {
   },[movieId]);
 
   useEffect(() => {
+    const sessionState = isPersistedState(movieId);
+    
+    if(sessionState) {
+      setState(sessionState);
+      setLoading(false);
+      return;
+    }
+
     fetchMovie();
   }, [movieId, fetchMovie]);
+
+  useEffect(() => {
+    sessionStorage.setItem(movieId,JSON.stringify(state));
+  }, [movieId, state]);
 
   return { state, loading, error };
 }
